d481fe7fff2b501e5e35f9e650598eaa040c1f0c
Alon Aviely <121253416+AAviely@users.noreply.github.com>
2026-02-19 00:18:53 +0200

    Update SharedMem_summary.c

--- SharedMem_summary.c
+++ SharedMem_summary.c
@@ -1,5 +1,14 @@
 #include <windows.h>
-
-int main(){
+#include <stdio.h>
+int main(int argc, char *argv[]){
+  if(argc!=0){
+    childProc(argc, argv);
+    return 0;
+  }
+  STARTUPINFO si[];
+  PROCESSINFO pi[];
+  ZeroMemory(&pi);
+  ZeroMemory(&si);
+  
   return 0;
 }

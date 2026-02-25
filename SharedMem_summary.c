3a14bdd2a76a978d98abbd4c47dd8867e75bcf4d
Alon Aviely <121253416+AAviely@users.noreply.github.com>
2026-02-25 22:21:05 +0200

    Update SharedMem_summary.c
    ?

--- SharedMem_summary.c
+++ SharedMem_summary.c
@@ -1,7 +1,8 @@
-#include <windows.h>
 #include <stdio.h>
-int main(int argc, char *argv[]){
-  if(argc!=0){
+#include <windows.h>
+
+int main(int argc, char *argv[]) {
+  if (argc != 0) {
     childProc(argc, argv);
     return 0;
   }
@@ -9,10 +10,10 @@ int main(int argc, char *argv[]){
   PROCESS_INFORMATION pi[2];
   ZeroMemory(&si, sizeof(si));
   ZeroMemory(&si);
-  for(int i = 0; i < 2; i++){
-      CreateProcess(
-              
-        );
+  for (int i = 0; i < 2; i++) {
+    CreateProcess(
+
+    );
   }
   return 0;
 }

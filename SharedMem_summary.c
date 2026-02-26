b630f9944bd44acac523d62e07c5a6fe3428474d
Alon Aviely <121253416+AAviely@users.noreply.github.com>
2026-02-26 13:12:55 +0200

    clang format and presets

--- SharedMem_summary.c
+++ SharedMem_summary.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <windows.h>
 
-int main(int argc, char *argv[]) {
-  if (argc != 0) {
-    childProc(argc, argv);
-    return 0;
-  }
-  STARTUPINFO si[2];
-  PROCESS_INFORMATION pi[2];
-  ZeroMemory(&si, sizeof(si));
-  ZeroMemory(&si);
-  for (int i = 0; i < 2; i++) {
-    CreateProcess(
+void childProc(int argc, char *argv[]) {}
 
-    );
-  }
-  return 0;
+int main(int argc, char *argv[]) {
+    if (argc != 0) {
+        childProc(argc, argv);
+        return 0;
+    }
+    STARTUPINFO si[2];
+    PROCESS_INFORMATION pi[2];
+    ZeroMemory(&si, sizeof(si));
+    ZeroMemory(&pi, sizeof(si));
+    si[0].cb = sizeof(si[0]);
+    si[1].cb = sizeof(si[1]);
+    int success[2];
+    for (int i = 0; i < 2; i++) {
+        success[i] = CreateProcess(
+
+        );
+    }
+    return 0;
 }
